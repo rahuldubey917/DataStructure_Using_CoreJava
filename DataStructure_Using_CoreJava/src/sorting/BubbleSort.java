5a56182ab696b0234baa1f41c60aaec6c82a48f0
Rahul Dubey <Rahul Dubey@192.168.0.46>
2018-08-09 11:46:47 +0530

    bubble sort

--- DataStructure_Using_CoreJava/src/sorting/BubbleSort.java
+++ DataStructure_Using_CoreJava/src/sorting/BubbleSort.java
@@ -1,10 +1,49 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
+		    // Initialize arr
+       Scanner sc=new Scanner(System.in);
+		 
+       System.out.println("enter size of array");
+		 int size=sc.nextInt();
+		 int[] arr=new int[size];
+		 
+		 System.out.println("enter the elements");
+		 
+		 
+		 for(int i=0;i<=size-1;i++)
+		 {
+			 arr[i]=sc.nextInt();
+		 }
+		 
+         System.out.print("Initial Array : ");
+		    printArray(arr);
+		    bubbleSort(arr);
+		  }
+		  public static void printArray(int[] arr) {
+		    for(int i = 0; i < arr.length; i++) {
+		      System.out.print(arr[i] + " ");
+		    }
 
-	}
+		    System.out.println();
+		  }
+		  static void bubbleSort(int[] arr) {
+		    for (int i = 0; i < arr.length; i++) {
+		      for (int j = 0; j < arr.length - i - 1; j++) {
+		        if (arr[j] > arr[j + 1]) {
+		          int temp = arr[j];
+		          arr[j] = arr[j + 1];
+		          arr[j + 1] = temp;
+		        }
+		      }
 
+		      System.out.print("After pass " + i + "  : ");
+		      //Printing array after pass
+		      printArray(arr);
+		    }
+		  }
 }
